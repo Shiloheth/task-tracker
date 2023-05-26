5297e670edc42db6dfe16c9ad853969ab1b75321
Shiloh <shilohofulue17@gmail.com>
2023-05-26 21:50:54 +0100

    Added additional state variables for functionality

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,18 @@ import { useEffect } from "react";
 export default function Home() {
   const [taskInput, setTaskInput] = useState<string>("");
   const [taskDetails, setTaskDetails] = useState<string[]>([]);
-  const [list, setList] = useState<any>([""]);
+  const [list, setList] = useState<any>([]);
   const [listName, setListName] = useState("");
-  const [activeList, setActiveList] = useState<any>(null);
+  const [activeList, setActiveList] = useState<any>(false);
   const [addTaskActive, setAddTaskActive] = useState(true);
   const [element, setElement] = useState(false);
+  const [selectedTask, setSelectedTask] = useState<any>("");
+
+  type ListItem = {
+    name: string;
+    content: string[];
+  };
+
   function handleinput(e: React.ChangeEvent<HTMLInputElement>) {
     setTaskInput(e.target.value);
   }
@@ -25,36 +32,39 @@ export default function Home() {
     console.log(list);
   }
 
-  console.log(activeList);
+  function selected(obj: any) {
+    setSelectedTask(obj);
+    console.log(obj);
+  }
 
   return (
     <div>
       <div className="grid grid-rows-1 grid-flow-col h-screen overflow-hidden">
-        {/* <div className="row-span-1 overflow-y-auto h-screen col-span-13 shadow-3xl bg-[#fff]">
-        <h1 className="w-40 text-center text-2xl flex flex-0 p-4">Task Tracker </h1>
-        <div className="w-40 flex items-center h-7 p-4">
-          <span>
-            <BsListTask className="text-2xl mr-2" />
-          </span>
-          <span className="text-base flex-1">Tasks</span>
+        <div className="hidden lg:block row-span-1 overflow-y-auto h-screen col-span-full shadow-3xl bg-[#DCDCDC]">
+          <h1 className=" text-2xl flex flex-0 mx-6 my-3 font-black inline-block">Task Tracker </h1>
+          <div className="flex items-center mx-6 mt-16">
+            <AiFillHome />
+            <h1 className="px-3 ">Tasks</h1>
+            <div className="ml-16 w-24 flex justify-end">{list.length}</div>
+          </div>
+          <div className="h-px my-3 mx-6 bg-[#111111]"></div>
+          <div className="w-80 ml-1">
+            <div className="truncate ml-1">
+              {list &&
+                list.map((listitem: ListItem, idx: number) => (
+                  <ul
+                    key={idx}
+                    className="flex flex-1 items-center w-full px-6 py-1 border-rounded hover:bg-[#C6C6C6]"
+                    onClick={() => selected(listitem)}
+                  >
+                    <li className="inline w-64 whitespace-normal">{listitem.name}</li>
+                    <li className="flex flex-1 justify-end">{listitem.content.length}</li>
+                  </ul>
+                ))}
+            </div>
+          </div>
         </div>
-        <div className="w-24" onClick={() => console.log()}>
-          <ul className="truncate w-12">
-            {list &&
-              list.map((item: any, idx: any) => (
-                <li key={idx} onClick={() => setActiveList(item)}>
-                  {item.content &&
-                    item.content.map((a: any) => {
-                      return null; // or the appropriate JSX element
-                    })}
-                  {item.name}
-                  {item.content}
-                </li>
-              ))}
-          </ul>
-        </div>
-      </div> */}
-        <div className="row-span-1 col-span-5 bg-[#f5f5f5] px-4">
+        <div className="row-span-1 col-span-12 bg-[#f5f5f5] px-4">
           <div className="h-16 w-full flex items-center">
             <div className="flex flex-1">
               <div className="flex items-center">
@@ -62,9 +72,13 @@ export default function Home() {
                   onClick={() => {
                     setElement(!element);
                   }}
-                  className="text-2xl focus"
+                  className="text-2xl lg:hidden"
                 />
-                <h1 className="px-10 font-black text-2xl">Task Tracker</h1>
+                <input
+                  placeholder="Search..."
+                  className="hidden lg:block w-full border-0 rounded mx-4 my-2 bg-transparent text-inherit focus:border-[#eaebed] p-2 focus:outline-none focus:border focus:bg-[#FAFAFA] focus:rounded-lg placeholder:text-inherit"
+                />
+                <h1 className="px-10 font-black text-2xl lg:hidden">Task Tracker</h1>
               </div>
 
               {/* <input
@@ -83,9 +97,16 @@ export default function Home() {
             </a>
           </div>
           <div className=""></div>
-          <div className="flex items-center justify-center h-full">
-            {false && (
-              <div className="flex flex-col fixed bottom-0 top-1/4 rounded-t-lg overflow-y-auto pt-1 pb-2 bg-gray-0 mx-auto max-w-2xl w-full  md:top-1/4 md:left-1/2 md:-translate-x-1/2 md:rounded-lg md:border md:border-black/10 md:dark:bg-gray-100 md:dark:border-white/20 md:bottom-auto">
+          <div className="flex items-center justify-center h-full relative">
+            {selectedTask ? (
+              <div className="h-full bg-[#f44336] absolute w-full">
+                <div className="text-2xl font-extrabold p-5">Swap</div>
+                <div className="bg-[#f2f2f2] m-4 p-2 font-medium">yo</div>
+                <div>test</div>
+              </div>
+            ) : null}
+            {activeList ? (
+              <div className="flex flex-col rounded-t-lg overflow-y-auto pt-1 pb-2 bg-gray-0 border border-black/10 w-full mb-16 ">
                 <input
                   placeholder="Task Name"
                   className="border-0 bg-transparent"
@@ -118,39 +139,44 @@ export default function Home() {
                 </div>
 
                 <div className="flex justify-end p-1">
-                  <button className="ml-2 bg-[#ff0021] px-4 py-2 rounded-md">Cancel</button>
+                  <button
+                    className="ml-2 bg-[#ff0021] px-4 py-2 rounded-md"
+                    onClick={() => setActiveList(false)}
+                  >
+                    Cancel
+                  </button>
                   <button className="ml-2 bg-[#5cdb5c] px-4 py-2 rounded-md" onClick={save}>
                     Save
                   </button>
                 </div>
               </div>
-            )}
-            <div className="flex flex-col items-center mb-16">
-              <p className="text-xl">Add Task</p>
-              <MdAdd className="text-5xl cursor-pointer" />
-            </div>
-
-            {activeList !== null ? (
-              <div>
-                <div>{activeList.name}</div>
-
-                {activeList.content.map((item: any, index: number) => (
-                  <div key={index}>{item}</div>
-                ))}
+            ) : (
+              <div
+                className="flex flex-col items-center mb-16 cursor-pointer"
+                onClick={() => {
+                  setActiveList(true);
+                }}
+              >
+                <p className="text-xl">Add Task</p>
+                <MdAdd className="text-5xl " />
               </div>
-            ) : null}
+            )}
           </div>
         </div>
       </div>
       {element && (
-        <div className="absolute w-96 h-full bg-[#DCDCDC] top-0">
+        <div className="absolute w-80 h-full bg-[#DCDCDC] top-0 lg:hidden">
           <div className="flex w-full">
             <input
               placeholder="Search..."
               className="w-full border-0 rounded mx-4 my-2 bg-transparent text-inherit focus:border-[#eaebed] p-2 focus:outline-none focus:border focus:bg-[#FAFAFA] focus:rounded-lg placeholder:text-inherit"
             />
           </div>
-          <div>Tasks</div>
+          <div className="flex items-center mx-6 mt-16">
+            <AiFillHome />
+            <h1 className="px-3">Tasks</h1>
+            <div className="flex grow justify-center"></div>
+          </div>
         </div>
       )}
     </div>
